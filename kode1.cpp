42d75b2b0e63ae840b385fff38bc1d425c9b187a
Zahwa-arya <@nabilahazna656@gmail.com>
2024-07-31 11:58:42 +0800

    Tugas selesai

--- kode1.cpp
+++ kode1.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 using namespace std;
 
+void cetakData(string nama,string nisn,string jurusan,int tinggibadan)
+{
+  cout << "nama anda" << nama << endl;
+  cout << "nisn anda" << nisn << endl;
+  cout << "jurusan anda" << jurusan << endl;
+  cout << "tinggi badan anda" << tinggibadan << endl;
+
+}
+
 int main() {
-    cout << "Selamat Datang di SMK TI Airlangga" << endl;
+    string nama = "Zahwa Aryafiah Nabila Hasna";
+    string nisn = "000008582024";
+    string jurusan = "PPLG";
+    int tinggibadan = 156;
+
+    cetakData(nama,nisn,jurusan,tinggibadan);
+
     return 0;
+
 }
